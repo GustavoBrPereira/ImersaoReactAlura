9463931315d6721113fe7d2f12102d2a67d3c365
GustavoBrPereira <gustavobritope@gmail.com>
2020-07-28 23:16:12 -0300

    Finalização da página home e criação das rotas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -6,12 +7,12 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt ="WoodyFlix logo" />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/" >
-                Children
+            <Button as={Link} className="ButtonLink" to="/cadastro/video" >
+                Novo vídeo
             </Button>
         </nav>
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+
+      <Route component={() => ( <div> Not Found. Error 404 </div> )} />
+
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
