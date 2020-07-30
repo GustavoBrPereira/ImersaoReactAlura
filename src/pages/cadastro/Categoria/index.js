f7e14cd6d0f42f8f5ada23ce4c2b737f4f91193b
GustavoBrPereira <gustavobritope@gmail.com>
2020-07-30 19:12:43 -0300

    Atualizando url do BDD

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -31,7 +31,9 @@ function CadastroCategoria() {
 
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/categorias'
+    : 'https://woodyflix.herokuapp.com/categorias';
     fetch(URL).then(
       async(respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
